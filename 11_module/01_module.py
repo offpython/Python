fad8eacc19239b489bb4c598aa026cd4cc048cbd
offpython <offpython@gmail.com>
2023-05-26 16:37:27 +0900

    update

---
+++ 11_module/01_module.py
@@ -0,0 +1,12 @@
+# 모듈
+## 이미 만들어진 훌륭한 기능으로 사용자가 쉽게 사용할 수 있다.
+
+# 실습1
+import random
+
+rNum = random.randint(1, 10)
+print(f'rNum = {rNum}')
+
+rNums = random.sample(range(0, 101), 10)
+print(f'rNums = {rNums}')
+

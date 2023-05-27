be8a1a69f44337f4a0b7f8ded62f1340ec434f5d
offpython <offpython@gmail.com>
2023-05-27 22:11:57 +0900

    update

---
+++ 12_file(main)/01_module.py
@@ -0,0 +1,21 @@
+# 실행(메인)파일 01
+
+## __name__에는 모듈 이름이 저장되거나 '__main__'이 저장된다.
+
+import addModule
+import subModule
+import mulModule
+import divModule
+
+def mod(n1, n2):
+    return n1 % n2
+
+if __name__ == '__main__':
+    print(addModule.add(10, 20))
+    print(subModule.sub(10, 20))
+    print(mulModule.mul(10, 20))
+    print(divModule.div(10, 20))
+
+
+
+

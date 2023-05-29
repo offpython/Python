c59ec858f1a6d2e25e803f9982060ba6bfb67f46
offpython <offpython@gmail.com>
2023-05-29 13:30:23 +0900

    update

--- 14_object/02_object.py
+++ 14_object/02_object.py
@@ -31,5 +31,31 @@ myPc.memory = '64G'
 myPc.ssd = '1t'
 myPc.printPCInfo()
 
+# 실습1
+class Calculator:
 
+    def __init__(self, num1, num2):
+        self.number1 = num1
+        self.number2 = num2
 
+    def add(self):
+        result = self.number1 + self.number2
+        return result
+
+    def sub(self):
+        result = self.number1 - self.number2
+        return result
+
+    def mul(self):
+        result = self.number1 * self.number2
+        return result
+
+    def div(self):
+        result = self.number1 / self.number2
+        return result
+
+newCarculator = Calculator(10, 20)
+print(f'newCarculator.add() : {newCarculator.add()}')
+print(f'newCarculator.sub() : {newCarculator.sub()}')
+print(f'newCarculator.mul() : {newCarculator.mul()}')
+print(f'newCarculator.div() : {newCarculator.div()}')

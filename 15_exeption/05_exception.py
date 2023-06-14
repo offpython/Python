3d95eebb0f76159e2b33df727e5018c411e6e219
offpython <offpython@gmail.com>
2023-06-14 18:06:32 +0900

    update

--- 15_exeption/05_exception.py
+++ 15_exeption/05_exception.py
@@ -16,21 +16,55 @@
 # print(f'num1 - num2 = {num1 - num2}')
 # print(f'num1 + num2 = {num1 + num2}')
 
-# raise
-## raise 키워드를 이용하면 예외를 발생시킬 수 있음
+# # raise
+# ## raise 키워드를 이용하면 예외를 발생시킬 수 있음
+#
+# def divCalculator(n1, n2):
+#
+#     if n2 != 0:
+#         print(f'{n1} / {n2} = {n1 / n2}')
+#     else:
+#         raise Exception('0으로 나눌 수 없습니다.')
+#
+# num1 = int(input('input number1 : '))
+# num2 = int(input('input number2 : '))
+#
+# try:
+#     divCalculator(num1, num2)
+#
+# except Exception as e:
+#     print(f'exception : {e}')
 
-def divCalculator(n1, n2):
+# 실습1
+## 사용자가 문자메세지를 보낼 떄 10글자 이하면 sms, 초과하면 mms 발송하는 프로그램
+def sendSMS(msg):
 
-    if n2 != 0:
-        print(f'{n1} / {n2} = {n1 / n2}')
+    if len(msg) > 10:
+        raise Exception('길이 초과! MMS 전환 후 발송!', 1)
     else:
-        raise Exception('0으로 나눌 수 없습니다.')
+        print('SMS 발송!')
 
-num1 = int(input('input number1 : '))
-num2 = int(input('input number2 : '))
+def sendMMS(msg):
+
+    if len(msg) <= 10:
+        raise Exception('길이 미달! SMS 전환 후 발송!', 2)
+    else:
+        print('MMS 발송!')
+
+
+msg = input('input message : ')
 
 try:
-    divCalculator(num1, num2)
+    sendSMS(msg)
 
 except Exception as e:
-    print(f'exception : {e}')
+    # args는 exception에 설정한 파라미터임. 배열형식이기 때문.
+    print(f'exception : {e.args[0]}')
+    print(f'exception : {e.args[1]}')
+
+    if e.args[1] == 1:
+        sendMMS(msg)
+    elif e.args[1] == 2:
+        sendSMS(msg)
+
+
---
+++ 15_exeption/06_exception.py
@@ -0,0 +1,6 @@
+# 사용자 Exception 클래스
+## Exception 클래스를 상속해서 사용자 예외 클래스를 만들 수 있다.
+
+class NotUseZeroException(Exception):
+
+    

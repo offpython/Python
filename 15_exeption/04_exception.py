e7b6380d678e7438353332e4705e1b71d40c081c
offpython <offpython@gmail.com>
2023-06-14 17:08:08 +0900

    update

---
+++ 15_exeption/04_exception.py
@@ -0,0 +1,57 @@
+# # finally
+# ## 예외 발생과 상관없이 항상 실행
+#
+# try:
+#     inputData = input('input number : ')
+#     numInt = int(inputData)
+#
+# except:
+#     print('exception raised')
+#     print('not number')
+#
+# else:
+#     if numInt % 2 == 0:
+#         print('even number!')
+#     else:
+#         print('odd number!')
+#
+# finally:
+#     print(f'input data: {inputData}')
+#
+# 실습1
+## 사용자로부터 숫자 5개 받아 짝수/홀수/실수와 입력한 모든 데이터를 각각 출력하는 프로그램
+
+eveList = []; oddList = []; floatList = []
+dataList = []
+
+n = 1
+while n < 6:
+
+    try:
+        data = input('input number : ')
+        floatNum = float(data)
+
+    except:
+        print('exception raised!')
+        print('input number again!')
+        continue
+
+    else:
+        if floatNum - int(floatNum) != 0:
+            print('float number!')
+            floatList.append(floatNum)
+        else:
+            if floatNum % 2 == 0:
+                print('even number!')
+                eveList.append(int(floatNum))
+            else:
+                print('odd number')
+                oddList.append(int(floatNum))
+
+        n += 1
+
+    finally:
+        dataList.append(data)
+
+print(f'data list: {dataList}')
+

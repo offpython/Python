47ae767499088abf660c8d8c9353a66f605e0648
offpython <offpython@gmail.com>
2023-06-15 14:37:18 +0900

    update

---
+++ 016_text/01_textFile.py
@@ -0,0 +1,24 @@
+# 텍스트 파일 쓰기
+
+file = open('/Users/soysauce/Documents/Study/python/PythonTxt/test.txt', 'w')
+
+strCnt = file.write('Hello PYTHON!')
+print(f'strCnt : {strCnt}')
+
+file.close()
+
+# 실습1
+
+import time
+
+lt = time.localtime()
+
+dateStr = '[' + str(lt.tm_year) + '년 ' + \
+          str(lt.tm_mon) + '월 ' \
+          + str(lt.tm_mday) + '일]'
+
+todaySchedule = input('오늘 일정 : ')
+
+file = open('/Users/soysauce/Documents/Study/python/PythonTxt/test.txt', 'w')
+file.write(dateStr + todaySchedule)
+file.close()

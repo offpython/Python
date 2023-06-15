f49a190b14bca9909fb73ad4c8564b2f0522f778
offpython <offpython@gmail.com>
2023-06-15 15:08:56 +0900

    update

--- 016_text/02_textFile.py
+++ 016_text/02_textFile.py
@@ -1,28 +1,28 @@
-# # 텍스트파일 읽기
-#
-# file = open('/Users/soysauce/Documents/Study/python/016_pythonTxt/test.txt', 'r')
-#
-# str = file.read()
-# print(f'str : {str}')
-#
-# file.close()
-#
-# # 실습1
-# import time
-#
-# lt = time.localtime()
-#
-# # dateStr = '[' + str(lt.tm_year) + '년 ' + \
-# #           str(lt.tm_mon) + '월 ' \
-# #           + str(lt.tm_mday) + '일]'
-#
-# dateStr = '[' + time.strftime('%Y-%m-%d %H:%M:%S %p') + ']'
-#
-# todaySchedule = input('오늘 일정 : ')
-#
-# file = open('/Users/soysauce/Documents/Study/python/016_pythonTxt/test.txt', 'w')
-# file.write(dateStr + todaySchedule)
-# file.close()
+# 텍스트파일 읽기
+
+file = open('/Users/soysauce/Documents/Study/python/016_pythonTxt/test.txt', 'r')
+
+str = file.read()
+print(f'str : {str}')
+
+file.close()
+
+# 실습1
+import time
+
+lt = time.localtime()
+
+# dateStr = '[' + str(lt.tm_year) + '년 ' + \
+#           str(lt.tm_mon) + '월 ' \
+#           + str(lt.tm_mday) + '일]'
+
+dateStr = '[' + time.strftime('%Y-%m-%d %H:%M:%S %p') + ']'
+
+todaySchedule = input('오늘 일정 : ')
+
+file = open('/Users/soysauce/Documents/Study/python/016_pythonTxt/test.txt', 'w')
+file.write(dateStr + todaySchedule)
+file.close()
 
 # 실습1
 ## 텍스트파일에서 'Python'을 '파이썬'으로 변경해서 다시 저장

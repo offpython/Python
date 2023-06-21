e2643f253f341cb12d89fc39dd8bb2dcc5342f62
offpython <offpython@gmail.com>
2023-06-21 09:17:28 +0900

    update

--- 17_testExample/06_test.py
+++ 17_testExample/06_test.py
@@ -1,12 +1,12 @@
-# 함수(5)
+# 함수(6)
 
-## 등차수열 공식 : an = a1 + (n-1) * d
-## 등차수열 합 공식 : sn = n * (a1 + an) / 2
+## 등비 수열(일반항) 공식 : an = a1 * r^(n-1)
+## 등비 수열(합) 공식 sn = a1 * r^(n-1) / (1-r)
 
-## 등차수열의 n번째 값과 합을 출력
-def sequenceCalculator(n1, d, n):
+## 등비수열의 n번째 값과 합을 출력
+def sequenceCal(n1, r, n):
 
-    valueN = 0; sumN = 0;
+    valueN = 0; sumN = 0
 
     i = 1
     while i <= n:
@@ -20,16 +20,15 @@ def sequenceCalculator(n1, d, n):
             i += 1
             continue
 
-        valueN += d
+        valueN *= r
         sumN += valueN
         print(f'{i}번째 항의 값 : {valueN}')
         print(f'{i}번째 항까지의 합 : {sumN}')
 
         i += 1
 
-inputN1 = int(input('a1 입력 :'))
-inputD = int(input('공차 입력 :'))
-inputN = int(input('n 입력 :'))
+inputN1 = int(input('a1 입력 : '))
+inputR = int(input('공비 입력 : '))
+inputN = int(input('n 입력 : '))
 
-sequenceCalculator(inputN1, inputD, inputN)
-
+sequenceCal(inputN1, inputR, inputN)
